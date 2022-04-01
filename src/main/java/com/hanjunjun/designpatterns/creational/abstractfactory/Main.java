7e2da37a44dd11d447ac76a9254cb16b272d9852
hanjunjun <1454055505@qq.com>
2022-04-01 20:10:28 +0800

    update

---
+++ src/main/java/com/hanjunjun/designpatterns/creational/abstractfactory/Main.java
@@ -0,0 +1,11 @@
+package com.hanjunjun.designpatterns.creational.abstractfactory;
+
+public class Main {
+	/**
+	 * 抽象工厂模式
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+}
---
+++ src/main/java/com/hanjunjun/designpatterns/creational/factorymethod/Main.java
@@ -0,0 +1,11 @@
+package com.hanjunjun.designpatterns.creational.factorymethod;
+
+public class Main {
+	/**
+	 * 工厂模式
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+}
